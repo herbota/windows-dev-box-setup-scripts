32ff35576d16e496935d09b29f03f523b2c1c3e3
Andy Herbota <andy.herbota@pointwest.com.ph>
2021-09-15 14:51:18 +0800

    Add Windows Terminal and IntelliJ IDEA

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,4 +1,6 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
+choco install -y microsoft-windows-terminal
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y intellijidea-community
