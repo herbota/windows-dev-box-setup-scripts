cbdda6f4ac73db89a0860b850a5b4fc1daa5e80c
Andy Herbota <andy.herbota@pointwest.com.ph>
2021-09-15 15:34:57 +0800

    Download and install Linux kernel update package

--- scripts/Docker.ps1
+++ scripts/Docker.ps1
@@ -2,3 +2,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName containers -All
 RefreshEnv
 choco install -y docker-for-windows
 choco install -y vscode-docker
+
+# download and install Linux kernel update package
+Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -Outfile $env:Temp\wsl_update_x64.msi
+Start-Process msiexec.exe -Wait -ArgumentList '/I $env:Temp\wsl_update_x64.msi /quiet'
