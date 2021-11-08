cdd25d5f592b054ec6d4bf5f9a447e05f140df42
Andy Herbota <andy@herbota.com>
2021-11-08 21:56:33 +0800

    Add 7-Zip

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -3,5 +3,6 @@
 choco install -y vscode
 choco install -y microsoft-windows-terminal
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y 7zip.install
 choco install -y intellijidea-community
 choco install -y visualstudio2019community --package-parameters "--add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended"
